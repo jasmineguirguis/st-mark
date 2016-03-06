c370f1d55be83f285eb09963abca8c23582f5389
Shaddy Mansour <shaddyhm@outlook.com>
2016-03-05 23:53:09 -0500

    Retrieve Gravatar profile pic

--- api/feeds.route.js
+++ api/feeds.route.js
@@ -1,9 +1,31 @@
 var express = require('express');
 var router = express.Router();
+var Promise = require('bluebird');
+var User = require('../mongoose/models/user.model');
+var gravatar = require('gravatar');
+
+function getProfilePicture(id){
+  return new Promise((resolve, reject) => {
+    User.findOne({ _id: id }, function(err, user){
+      if (err) reject(err);
+      var email = user.email;
+      var profilePicture = gravatar.url(email, {s: '50'});
+      resolve(profilePicture);
+    })
+  })
+}
 
 router.route('/')
   .get(function(req, res){
-    res.status(200).send({ message: 'AUTH PASS SUCCESS' });
+    
+    var userId = req.userId;
+    getProfilePicture(userId)
+      .then((profilePicture) => {
+        res.status(200).send({ profilePicture: profilePicture });
+      }).catch((err) => {
+        res.status(500).send({ message: 'Something went wrong retrieving user info.', error: err });
+      });
+
   });
 
 module.exports = router;
